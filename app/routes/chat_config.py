27653a9844e0304491fe74a92f919b023e8a3c65
Amile-Wang <1420947175@qq.com>
2026-01-03 18:19:22 +0800

    更新唤醒词

--- app/routes/chat_config.py
+++ app/routes/chat_config.py
@@ -48,17 +48,6 @@ async def get_server_base_config():
             },
             "wakeup_words": [
                 "捷宝宝",
-                "你好小智",
-                "你好小志",
-                "小爱同学",
-                "你好小鑫",
-                "你好小新",
-                "小美同学",
-                "小龙小龙",
-                "喵喵同学",
-                "小滨小滨",
-                "小冰小冰",
-                "嘿你好呀"
             ],
             "selected_module": {
                 "ASR": "ASR_FunASR",
